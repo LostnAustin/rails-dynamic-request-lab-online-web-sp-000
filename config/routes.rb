f0be6e11060c83de046962e811bb4f4b0f57f3f2
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-14 19:07:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :students, only: :index
 
   resources :students, only: :show
-
+    # below replaced with above code
   # get 'students/:id', to: 'students#show'
 
 end
